5500b931fad14ee23fb9927b2cff6ca22dca6217
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2019-04-30 03:16:51 -0300

    Correção para tornar o cache realmente thread-safe

--- src/main/java/br/com/fiap/trabalhofinal/repositories/TransactionRepository.java
+++ src/main/java/br/com/fiap/trabalhofinal/repositories/TransactionRepository.java
@@ -4,48 +4,64 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
 import br.com.fiap.trabalhofinal.vo.StatisticVO;
 import br.com.fiap.trabalhofinal.vo.TransactionVO;
 
 @Repository
+@EnableScheduling
 public class TransactionRepository {
 
 	private Set<TransactionVO> cache = Collections.synchronizedSet(new HashSet<>());
 
 	public void saveTransaction(TransactionVO transaction) {
-		cache.add(transaction);
+		synchronized (cache) {
+			cache.add(transaction);
+		}
 	}
 
 	public StatisticVO getStatistics() {
 		StatisticVO statistics = new StatisticVO();
 
-		if (cache.size() > 0) {
-			statistics.setSum(0d);
+		synchronized (cache) {
+			if (cache.size() > 0) {
+				statistics.setSum(0d);
 
-			cache.stream().forEach(c -> {
-				Double amount = c.getAmount();
+				cache.stream().forEach(c -> {
+					Double amount = c.getAmount();
 
-				Double sum = statistics.getSum();
-				Double min = statistics.getMin();
-				Double max = statistics.getMax();
+					Double sum = statistics.getSum();
+					Double min = statistics.getMin();
+					Double max = statistics.getMax();
 
-				statistics.setSum(sum + amount);
+					statistics.setSum(sum + amount);
 
-				if (min == null || amount < min)
-					statistics.setMin(amount);
+					if (min == null || amount < min)
+						statistics.setMin(amount);
 
-				if (max == null || amount > max)
-					statistics.setMax(amount);
+					if (max == null || amount > max)
+						statistics.setMax(amount);
 
-				statistics.setCount(statistics.getCount() + 1);
-			});
+					statistics.setCount(statistics.getCount() + 1);
+				});
 
-			statistics.setAvg(statistics.getSum() / statistics.getCount());
+				statistics.setAvg(statistics.getSum() / statistics.getCount());
+			}
 		}
 
 		return statistics;
+	}
+
+	@Scheduled(fixedDelay = 1)
+	private void cleanOldDates() {
+		long timeMillis = System.currentTimeMillis() - 60000;
+
+		synchronized (cache) {
+			cache.removeIf(c -> c.getTimestamp() < timeMillis);
+		}
 	}
 
 }
